7886d889d9f7f162783f0863ad0c8324e837f57f
Charles Farris <charlesfarris71@gmail.com>
2025-01-26 22:37:50 -0700

    Add ToDoListLine component

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,17 +1,22 @@
 import { JSX } from "react";
-import { ToDoItem } from "./ToDoItem";
+import type { ToDoItem } from "./ToDoItem";
+import { ToDoListLine } from "./ToDoListLine";
 
 export interface ToDoListProps {
   items: ToDoItem[];
 }
 
 export function ToDoList(props: ToDoListProps): JSX.Element {
+  function onClick(item: ToDoItem): void {
+    alert(item.name);
+  }
+
   return (
     <>
       <h1>To-Do Items</h1>
       <ul>
         {props.items.map((item) => {
-          return <li key={item.id}>{item.name}</li>;
+          return <ToDoListLine key={item.id} item={item} onClick={onClick} />;
         })}
       </ul>
     </>
